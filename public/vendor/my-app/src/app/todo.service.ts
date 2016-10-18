d9e2d561d61a4adc7c537c37f79850438f8b6eac
Chad Hollman <hollmanchad@gmail.com>
2016-10-18 16:16:59 -0700

    old template files and files that were replaced

--- public/vendor/my-app/src/app/todo.service.ts
+++ public/vendor/my-app/src/app/todo.service.ts
@@ -7,26 +7,24 @@ export class TodoService {
 
   constructor(public http: Http) { }
   getAll() {
-    return this.http.get('http://localhost:3000/api/v1/todos');
+    return this.http.get('api/v1/todos');
   }
 
   save(todo) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('Access-Control-Allow-Origin', 'localhost:4200');
-    return this.http.post('http://localhost:3000/api/v1/todo', JSON.stringify(todo), {headers: headers})
+    return this.http.post('api/v1/todo', JSON.stringify(todo), {headers: headers})
       .map(res => res.json());
   }
 
   update(todo) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('Access-Control-Allow-Origin', 'localhost:4200');
-    return this.http.put('http://localhost:3000/api/v1/todo/' + todo._id, JSON.stringify(todo), {headers: headers});
+    return this.http.put('api/v1/todo/' + todo._id, JSON.stringify(todo), {headers: headers});
   }
 
   delete (id) {
-    return this.http.delete('http://localhost:3000/api/v1/todo/' + id);
+    return this.http.delete('api/v1/todo/' + id);
   }
 
 }
--- public/vendor/temp/e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class MyAppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('my-app-app h1')).getText();
-  }
-}
